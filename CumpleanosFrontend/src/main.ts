d392cd7292f2d763166e04967cc31a07812d92af
SmelDefart <inakimanuel98@gmail.com>
2024-05-13 20:03:59 -0300

    error de imports

--- CumpleanosFrontend/src/main.ts
+++ CumpleanosFrontend/src/main.ts
@@ -3,7 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { ConfigService } from './app/config/config.service';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 
 bootstrapApplication(AppComponent, appConfig)
